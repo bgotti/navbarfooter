efbd1fb87552369d41a44e764d911a7f961cd28e
Brian Gotti <briandgotti@gmail.com>
2020-10-11 16:59:18 -0400

    commit

---
+++ navbar/index.js
@@ -0,0 +1,13 @@
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav__links');
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav__active');
+    burger.classList.toggle('toggle');
+  });
+
+
+}
+
+navSlide();
